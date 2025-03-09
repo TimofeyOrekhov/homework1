309e993f5d533617c4dbdfdb9fdb4399ad36bbc4
Timofey Orekhov <drexweblancer@yandex.ru>
2025-03-09 11:20:37 +0300

    Выполнил задания с 9 по 10

--- scripts/hw-7.js
+++ scripts/hw-7.js
@@ -83,6 +83,41 @@ function getRandomNumberInRange(min, max) {
 
 console.log(getRandomNumberInRange(5, 28));
 
-// Задание 8
-let currentDate = new Date().toLocaleDateString('ru-RU');
-console.log(currentDate);
+// Задание 8, 9 
+
+let currentDate = new Date();
+
+console.log(currentDate.toLocaleDateString('ru-RU'));
+
+let futureDate = new Date(currentDate);
+futureDate.setDate(currentDate.getDate() + 73);
+
+console.log(futureDate.toLocaleDateString('ru-RU'));
+
+// Задание 10
+function formatDate(date) {
+    const months = [
+        "января", "февраля", "марта", "апреля", "мая", "июня",
+        "июля", "августа", "сентября", "октября", "ноября", "декабря"
+    ];
+    const days = [
+        "воскресенье", "понедельник", "вторник", "среда",
+        "четверг", "пятница", "суббота"
+    ];
+
+    
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const dayOfWeek = days[date.getDay()]; 
+
+
+    const hours = String(date.getHours()).padStart(2, '0'); 
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    
+    return `Дата: ${day} ${month} ${year} — это ${dayOfWeek}.\nВремя: ${hours}:${minutes}:${seconds}`;
+}
+
+currentDate = new Date();
+console.log(formatDate(currentDate));
